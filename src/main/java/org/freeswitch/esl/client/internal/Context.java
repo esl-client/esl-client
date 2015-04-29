86fa9cf826e545cd13d439ff61dfd7df2029d0cd
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-04-30 01:40:50 +0200

    Context: Fix typo in sendBackgroundApiCommand() method

--- src/main/java/org/freeswitch/esl/client/internal/Context.java
+++ src/main/java/org/freeswitch/esl/client/internal/Context.java
@@ -80,7 +80,7 @@ public class Context implements IModEslApi {
 			sb.append(' ').append(arg);
 		}
 
-		return handler.sendBackgroundApiCommand(channel, command);
+		return handler.sendBackgroundApiCommand(channel, sb.toString());
 	}
 
 	/**
