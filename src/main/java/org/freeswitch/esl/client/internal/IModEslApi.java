d702003fa3d482b27d63150749f3ab703eee9b7a
Tobias Bieniek <tobias.bieniek@gmx.de>
2015-04-30 01:34:48 +0200

    Remove redundant modifiers from interfaces

--- src/main/java/org/freeswitch/esl/client/internal/IModEslApi.java
+++ src/main/java/org/freeswitch/esl/client/internal/IModEslApi.java
@@ -8,7 +8,7 @@ import org.freeswitch.esl.client.transport.message.EslMessage;
 
 public interface IModEslApi {
 
-	public enum EventFormat {
+	enum EventFormat {
 
 		PLAIN("plain"),
 		XML("xml");
@@ -28,7 +28,7 @@ public interface IModEslApi {
 
 	;
 
-	public enum LoggingLevel {
+	enum LoggingLevel {
 
 		CONSOLE("console"),
 		DEBUG("debug"),
